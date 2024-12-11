7301c37f1ea5f363462f0ef5966a590d162f74fc
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2024-12-11 17:15:58 +0300

    jdbc

---
+++ JDBC/src/main/java/org/example/Db.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Db {
+    private static final String URL = "jdbc:mysql://localhost:3306";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static void con() {
+        try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
+
+        } catch (SQLException e){
+            throw new RuntimeException();
+        }
+    }
+}
