f8a5ff5261e732594f03e5f770409c14a2294830
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2024-12-12 15:41:52 +0300

    jdbc

--- JDBC/src/main/java/org/example/Db.java
+++ JDBC/src/main/java/org/example/Db.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Db {
     private static final String URL = "jdbc:mysql://localhost:3306";
@@ -11,10 +9,32 @@ public class Db {
 
     public static void con() {
         try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
-            System.out.println("Connection done!");
+            Statement statement = con.createStatement();
+            System.out.println("Statement done!");
 
-        } catch (SQLException e){
+            statement.execute("DROP SCHEMA `test` ;");
+            statement.execute("CREATE SCHEMA `test` ;");
+
+            statement.execute("CREATE TABLE `test`.`table` (\n" +
+                    " `id` INT NOT NULL,\n" +
+                    " `firstname` VARCHAR(45) NULL,\n" +
+                    " `lastname` VARCHAR(45) NULL,\n" +
+                    " PRIMARY KEY (`id`));");
+
+            statement.execute("INSERT INTO `test`.`table` (`id`,`firstname`,`lastname`)\n"
+                    + "VALUES (1,'Иванов','Иван');");
+            statement.execute("INSERT INTO `test`.`table` (`id`,`firstname`,`lastname`)\n" +
+            "VALUES (2,'Петров','Пётр');");
+
+            ResultSet set = statement.executeQuery("SELECT * FROM `test`.`table`;");
+
+            while (set.next()){
+                System.out.println(set.getString(3) + " " + set.getString(2) + " " + set.getInt(1));
+            }
+        } catch (SQLException e) {
             throw new RuntimeException();
         }
+
+
     }
 }
